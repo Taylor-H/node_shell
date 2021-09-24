0fa88b48372fce5b191af396395acab6a4f70c0e
Taylor H <taylor@htmlartist.net>
2021-09-24 15:48:42 -0400

    added pwd ability

--- bash.js
+++ bash.js
@@ -1,17 +1,26 @@
+
 //output a prompt
 process.stdout.write('prompt > ');
 
 //The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();// remove the newline
+if(cmd === 'pwd'){
+    process.stdout.write(process.cwd())
+    process.stdout.write('\nprompt > ');
+}else{
 
     process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ');
+}
+
 });
 
 
 // we can access the pwd function and store it in a variable like so
-const pwd = require('./pwd');
+// const pwd = require('./pwd');
 
 // // if a user enters 'pwd' as an argument, we can then call it
 // pwd();
+// console.log(`Current directory: ${cwd()}`);
+// process.cwd()
--- pwd.js
+++ pwd.js
@@ -1,3 +1,3 @@
 module.exports = function () {
-    // pwd code
+    process.cwd()
 };
